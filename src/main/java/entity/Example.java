3b5dbdf95cbdbc72ef395bead4665904306fcc5f
CecilJxSu <957005719@qq.com>
2016-08-18 18:28:32 +0800

    Add example with RESTful style

---
+++ src/main/java/entity/Example.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Example {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
