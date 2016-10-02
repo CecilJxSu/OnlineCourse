d51dbeae9ee3b54bccc15d29b3c421fa6d78def9
ceciljxsu <ceciljxsu@gmail.com>
2016-10-02 18:26:57 +0800

    Add answer, catalog

--- src/main/java/cn.canlnac.course/dao/AnswerDao.java
+++ src/main/java/cn.canlnac.course/dao/AnswerDao.java
@@ -1,10 +1,58 @@
 package cn.canlnac.course.dao;
 
+import cn.canlnac.course.entity.Answer;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
- * Created by cecil on 2016/9/20.
+ * 回答接口
  */
 @Component
 public interface AnswerDao {
+    /**
+     * 创建回答
+     * @param answer    回答
+     * @return          成功创建数目
+     */
+    int create(Answer answer);
+
+    /**
+     * 更新回答
+     * @param answer    回答
+     * @return          成功更新数目
+     */
+    int update(Answer answer);
+
+    /**
+     * 获取回答
+     * @param catalogId 章节ID
+     * @param userId    用户ID
+     * @return          回答
+     */
+    Answer getAnswer(
+            @Param("catalogId") int catalogId,
+            @Param("userId") int userId
+    );
+
+    /**
+     * 统计用户的回答数
+     * @param userId    用户ID
+     * @return          回答数
+     */
+    int count(int userId);
+
+    /**
+     * 获取用户回答
+     * @param start     分页开始位置
+     * @param count     分页返回数目
+     * @param userId    用户ID
+     * @return
+     */
+    List<Answer> getAnswers(
+            @Param("start") int start,
+            @Param("count") int count,
+            @Param("userId") int userId
+    );
 }
--- src/main/java/cn.canlnac.course/service/impl/CatalogServiceImpl.java
+++ src/main/java/cn.canlnac.course/service/impl/CatalogServiceImpl.java
@@ -1,17 +1,60 @@
 package cn.canlnac.course.service.impl;
 
 import cn.canlnac.course.dao.CatalogDao;
+import cn.canlnac.course.entity.Catalog;
 import cn.canlnac.course.service.CatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
- * Created by cecil on 2016/9/20.
+ * 课程章节事务接口实现
  */
 @Transactional
 @Component(value = "CatalogService")
 public class CatalogServiceImpl implements CatalogService {
     @Autowired
     private CatalogDao catalogDao;
+
+    /**
+     * 创建章节
+     * @param catalogs  章节数组
+     * @return          成功创建数目
+     */
+    @Override
+    public int create(Catalog[] catalogs) {
+        return catalogDao.create(catalogs);
+    }
+
+    /**
+     * 更新章节
+     * @param catalog   章节
+     * @return          成功更新数目
+     */
+    @Override
+    public int update(Catalog catalog) {
+        return catalogDao.update(catalog);
+    }
+
+    /**
+     * 获取课程下的所有章节
+     * @param courseId  课程ID
+     * @return          章节列表
+     */
+    @Override
+    public List<Catalog> getList(int courseId) {
+        return catalogDao.getList(courseId);
+    }
+
+    /**
+     * 删除章节
+     * @param id    章节ID
+     * @return      成功删除数目
+     */
+    @Override
+    public int delete(int id) {
+        return catalogDao.delete(id);
+    }
 }
