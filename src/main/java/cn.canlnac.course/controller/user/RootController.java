35e1b56d0e966ae852a54646620b334d0aaa1494
ceciljxsu <ceciljxsu@gmail.com>
2016-10-06 23:25:30 +0800

    Add root controller and test and validator

--- src/main/java/cn.canlnac.course/controller/user/RootController.java
+++ src/main/java/cn.canlnac.course/controller/user/RootController.java
@@ -1,7 +1,17 @@
 package cn.canlnac.course.controller.user;
 
+import cn.canlnac.course.entity.User;
+import cn.canlnac.course.service.UserService;
+import cn.canlnac.course.util.JWT;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 用户
@@ -9,4 +19,138 @@ import org.springframework.web.bind.annotation.RestController;
 @Component("UserRootController")
 @RestController
 public class RootController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JWT jwt;
+
+    /**
+     * 用户注册
+     * @param body  请求数据
+     * @return      用户ID
+     */
+    @PostMapping("/user")
+    public ResponseEntity<Map<String, Object>> register(@RequestBody Map<String, Object> body) {
+        //检查参数是否合法
+        if(body.get("username") == null || body.get("password") == null ||
+                !EmailValidator.getInstance().isValid(body.get("email").toString()))
+        {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        //创建用户
+        User user = new User();
+        //设置用户的信息
+        user.setUsername(body.get("username").toString());
+        user.setPassword(body.get("password").toString());
+        user.setPassword(body.get("email").toString());
+
+        //创建用户
+        int createdCount = userService.create(user);
+
+        //创建数目不为1，应该返回500，内部错误
+        if (createdCount != 1){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        //创建返回数据
+        HashMap<String,Object> sendData = new HashMap();
+        //设置返回数据，用户ID
+        sendData.put("userID", user.getId());
+
+        //返回注册用户数据
+        return new ResponseEntity(sendData, HttpStatus.OK);
+    }
+
+    /**
+     * 修改密码
+     * @param Authentication    登录信息
+     * @param body              请求数据
+     * @return                  空对象
+     */
+    @PutMapping("/user")
+    public ResponseEntity<Map<String, Object>> changePwd(
+            @RequestHeader(value="Authentication", required = false) String Authentication,
+            @RequestBody Map<String, Object> body
+    ) {
+        //检查参数是否合法
+        if(body.get("old") == null || body.get("new") == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        Map<String,Object> auth;
+        //未登录
+        if(Authentication == null || (auth = jwt.decode(Authentication)) == null){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
+        //获取用户
+        User user = userService.findByID(Integer.parseInt(auth.get("id").toString()));
+        //验证密码
+        if(!user.getPassword().equals(body.get("old").toString())){
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
+
+        //修改密码
+        user.setPassword(body.get("new").toString());
+        //更新密码
+        int updatedCount = userService.update(user);
+
+        //更新不成功
+        if(updatedCount != 1){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        //创建返回数据
+        HashMap<String,Object> sendData = new HashMap();
+
+        //返回空对象
+        return new ResponseEntity(sendData, HttpStatus.OK);
+    }
+
+    /**
+     * 永久封号
+     * @param Authentication    登录信息
+     * @param userId            用户ID
+     * @return                  空对象
+     */
+    @DeleteMapping("/user/{userId}")
+    public ResponseEntity<Map<String, Object>> deleteUser(
+            @RequestHeader(value="Authentication", required = false) String Authentication,
+            @PathVariable int userId
+    ) {
+        //未登录
+        Map<String,Object> auth;
+        if(Authentication == null || (auth = jwt.decode(Authentication)) == null){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+        //不是管理员
+        if (!auth.get("userStatus").toString().equals("admin")) {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
+
+        //获取用户
+        User user = userService.findByID(userId);
+        //用户不存在
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        //设置永久封号
+        user.setStatus("dead");
+        //更新用户状态
+        int updatedCount = userService.update(user);
+
+        //更新不成功
+        if(updatedCount != 1){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        //创建返回数据
+        HashMap<String,Object> sendData = new HashMap();
+
+        //返回空对象
+        return new ResponseEntity(sendData, HttpStatus.OK);
+    }
 }
--- src/test/java/cn/canlnac/course/controller/user/RootControllerTest.java
+++ src/test/java/cn/canlnac/course/controller/user/RootControllerTest.java
@@ -1,13 +1,31 @@
 package cn.canlnac.course.controller.user;
 
+import cn.canlnac.course.entity.User;
+import cn.canlnac.course.service.UserService;
+import cn.canlnac.course.util.JWT;
+import org.apache.commons.collections.map.HashedMap;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * 用户测试用例
@@ -21,4 +39,548 @@ public class RootControllerTest {
 
     @LocalServerPort
     private int port;
+
+    private String url;
+
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private JWT jwt;
+
+    /**
+     * 注册用户，邮箱格式错误，应该返回400
+     */
+    @Test
+    public void shouldReturn400WhenRegister() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("username","123");
+        body.put("password","123");
+        body.put("email","123");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class );
+
+        //返回状态码应该等于400
+        assertEquals(400,response.getStatusCode().value());
+    }
+
+    /**
+     * 注册用户，创建用户失败，应该返回500
+     */
+    @Test
+    public void shouldReturn500WhenRegister() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("username","123");
+        body.put("password","123");
+        body.put("email","123@qq.com");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //Mock userService，创建用户
+        when(this.userService.create(any())).thenReturn(0);  //返回创建数目
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class );
+
+        //返回状态码应该等于500
+        assertEquals(500,response.getStatusCode().value());
+    }
+
+    /**
+     * 注册用户成功，应该返回200和用户ID
+     */
+    @Test
+    public void shouldReturn200AndUserIdWhenRegister() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("username","123");
+        body.put("password","123");
+        body.put("email","123@qq.com");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //Mock userService，创建用户
+        when(this.userService.create(any())).thenReturn(1);  //返回创建数目
+        //设置用户ID
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class );
+
+        //返回状态码应该等于200
+        assertEquals(200,response.getStatusCode().value());
+        //body数据，返回用户ID
+        assertEquals("{\"userID\":0}",response.getBody());
+    }
+
+    /**
+     * 修改密码，参数错误，应该返回400
+     */
+    @Test
+    public void shouldReturn400WhenChangePwd() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("old","123");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class );
+
+        //返回状态码应该等于400
+        assertEquals(400,response.getStatusCode().value());
+    }
+
+    /**
+     * 修改密码，用户未登录，应该返回401
+     */
+    @Test
+    public void shouldReturn401WhenChangePwd() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("old","123");
+        body.put("new","321");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class );
+
+        //返回状态码应该等于401
+        assertEquals(401,response.getStatusCode().value());
+    }
+
+    /**
+     * 修改密码，用户未登录，应该返回401
+     */
+    @Test
+    public void shouldReturn401WithDecodeJwtWhenChangePwd() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("old","123");
+        body.put("new","321");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(null);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class );
+
+        //返回状态码应该等于401
+        assertEquals(401,response.getStatusCode().value());
+    }
+
+    /**
+     * 修改密码，用户密码错误，应该返回403
+     */
+    @Test
+    public void shouldReturn403WhenChangePwd() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("old","123");
+        body.put("new","321");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("id",1);
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //查找用户，返回用户信息
+        User user = new User();
+        user.setPassword("abc");    //密码错误
+        //Mock userService，获取用户信息
+        when(userService.findByID(1)).thenReturn(user);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class );
+
+        //返回状态码应该等于403
+        assertEquals(403,response.getStatusCode().value());
+    }
+
+    /**
+     * 修改密码，修改失败，应该返回500
+     */
+    @Test
+    public void shouldReturn500WhenChangePwd() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("old","123");
+        body.put("new","321");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("id",1);
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //查找用户，返回用户信息
+        User user = new User();
+        user.setPassword("123");
+        //Mock userService，获取用户信息
+        when(userService.findByID(1)).thenReturn(user);
+        when(userService.update(any())).thenReturn(0);  //更新失败
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class );
+
+        //返回状态码应该等于500
+        assertEquals(500,response.getStatusCode().value());
+    }
+
+    /**
+     * 修改密码，修改成功，应该返回200和空对象
+     */
+    @Test
+    public void shouldReturn200WhenChangePwd() {
+        url = "http://localhost:"+port+"/user";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+        body.put("old","123");
+        body.put("new","321");
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("id",1);
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //查找用户，返回用户信息
+        User user = new User();
+        user.setPassword("123");
+        //Mock userService，获取用户信息
+        when(userService.findByID(1)).thenReturn(user);
+        when(userService.update(any())).thenReturn(1);  //更新成功
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class );
+
+        //返回状态码应该等于200
+        assertEquals(200,response.getStatusCode().value());
+        //返回空对象
+        assertEquals("{}",response.getBody().toString());
+    }
+
+    /**
+     * 永久封号，参数错误，应该返回400
+     */
+    @Test
+    public void shouldReturn400WhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/abc"; //用户ID错误
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于400
+        assertEquals(400,response.getStatusCode().value());
+    }
+
+    /**
+     * 永久封号，用户未登录，应该返回401
+     */
+    @Test
+    public void shouldReturn401WhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/12";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于401
+        assertEquals(401,response.getStatusCode().value());
+    }
+
+    /**
+     * 永久封号，用户未登录，jwt解码为空，应该返回401
+     */
+    @Test
+    public void shouldReturn401WithDecodeJwtWhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/12";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        when(jwt.decode(any())).thenReturn(null);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于401
+        assertEquals(401,response.getStatusCode().value());
+    }
+
+    /**
+     * 永久封号，登录用户不是管理员，应该返回403
+     */
+    @Test
+    public void shouldReturn403WhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/12";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("userStatus","teacher");   //不是管理员
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于403
+        assertEquals(403,response.getStatusCode().value());
+    }
+
+    /**
+     * 永久封号，登录用户是管理员，封号用户不存在，应该返回404
+     */
+    @Test
+    public void shouldReturn404WhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/12";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("userStatus","admin");   //是管理员
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //Mock userService，获取用户信息
+        when(userService.findByID(1)).thenReturn(null); //用户不存在
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于404
+        assertEquals(404,response.getStatusCode().value());
+    }
+
+    /**
+     * 永久封号，封号失败，应该返回500
+     */
+    @Test
+    public void shouldReturn500WhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/12";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("userStatus","admin");   //是管理员
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //查找用户，返回用户信息
+        User user = new User();
+        //Mock userService，获取用户信息
+        when(userService.findByID(12)).thenReturn(user); //用户存在
+
+        //Mock userService，更新用户信息
+        when(userService.update(any())).thenReturn(0);  //更新失败
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于500
+        assertEquals(500,response.getStatusCode().value());
+    }
+
+    /**
+     * 永久封号，封号成功，应该返回200并返回空对象
+     */
+    @Test
+    public void shouldReturn200WhenDeleteUser() {
+        url = "http://localhost:"+port+"/user/12";
+
+        //创建body
+        Map<String,Object> body = new HashMap();
+
+        //创建headers
+        MultiValueMap<String,String> requestHeaders = new LinkedMultiValueMap<>();
+        //设置headers
+        requestHeaders.add("Content-Type","application/json; charset=UTF-8");
+        //头部登录信息
+        requestHeaders.add("Authentication","123asd");
+
+        //设置http请求数据
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(body, requestHeaders);
+
+        //解码返回的用户信息
+        Map<String,Object> auth = new HashedMap();
+        auth.put("userStatus","admin");   //是管理员
+        //Mock jwt，解码json web token
+        when(jwt.decode(any())).thenReturn(auth);
+
+        //查找用户，返回用户信息
+        User user = new User();
+        //Mock userService，获取用户信息
+        when(userService.findByID(12)).thenReturn(user); //用户存在
+
+        //Mock userService，更新用户信息
+        when(userService.update(any())).thenReturn(1);  //更新成功
+
+        //发起请求
+        ResponseEntity response = this.restTemplate.exchange(url, HttpMethod.DELETE, httpEntity, String.class );
+
+        //返回状态码应该等于200
+        assertEquals(200,response.getStatusCode().value());
+        //返回空对象
+        assertEquals("{}",response.getBody().toString());
+    }
 }
