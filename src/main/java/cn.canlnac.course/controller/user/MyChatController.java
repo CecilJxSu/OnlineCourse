a1df73738b931491e5d0e4b9da3ed01db222c321
= <=>
2017-02-08 16:36:05 +0800

    Fix myChats.

--- src/main/java/cn.canlnac.course/controller/user/MyChatController.java
+++ src/main/java/cn.canlnac.course/controller/user/MyChatController.java
@@ -1,8 +1,13 @@
 package cn.canlnac.course.controller.user;
 
 import cn.canlnac.course.entity.Chat;
+import cn.canlnac.course.entity.Profile;
 import cn.canlnac.course.service.ChatService;
+import cn.canlnac.course.service.FavoriteService;
+import cn.canlnac.course.service.LikeService;
+import cn.canlnac.course.service.ProfileService;
 import cn.canlnac.course.util.JWT;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 我的话题
@@ -25,6 +28,12 @@ public class MyChatController {
 
     @Autowired
     private ChatService chatService;
+    @Autowired
+    private ProfileService profileService;
+    @Autowired
+    private LikeService likeService;
+    @Autowired
+    private FavoriteService favoriteService;
 
     @Autowired
     private JWT jwt;
@@ -61,17 +70,76 @@ public class MyChatController {
         }
 
         //获取话题
-        List<Chat> chats = chatService.getList(
+        List<Chat> chatList = chatService.getList(
             start,
             count,
             "date",
             conditions
         );
 
+        //返回的话题
+        List<Map<String,Object>> chats = new ArrayList<>();
+
+        //遍历话题
+        Iterator<Chat> iterator = chatList.iterator();
+        Chat chat;
+        while (iterator.hasNext()){
+            chat = iterator.next();
+            Map<String,Object> chatObj = new HashMap<>();
+
+            //设置返回信息
+            chatObj.put("id",chat.getId());
+            chatObj.put("date",chat.getDate());
+            chatObj.put("title",chat.getTitle());
+            chatObj.put("content",chat.getContent());
+            chatObj.put("html",chat.getHtml());
+
+            //设置作者
+            Profile profile = profileService.findByUserID(chat.getUserId());
+            if(profile != null){
+                Map<String,Object> author = new HashMap<>();
+
+                author.put("id",profile.getUserId());
+                author.put("name",profile.getNickname());
+                author.put("iconUrl",profile.getIconUrl());
+
+                chatObj.put("author", author);
+            }
+
+            //话题图片
+            List<String> pictureUrls = new ArrayList<>();
+            if (chat.getPictureUrls() != null){
+                JSONArray array = new JSONArray(chat.getPictureUrls());
+                for (int i = 0; i < array.length(); i++){
+                    pictureUrls.add(array.get(i).toString());
+                }
+            }
+            chatObj.put("pictureUrls", pictureUrls);
+
+            chatObj.put("watchCount",chat.getWatchCount());
+            chatObj.put("likeCount",chat.getLikeCount()/2);
+            chatObj.put("commentCount",chat.getCommentCount()/3);
+            chatObj.put("favoriteCount",chat.getFavoriteCount()/4);
+
+            int isLike = 0;
+            int isFavorite = 0;
+            //用户登录
+            if (auth != null){
+                //获取标记
+                isLike = likeService.isLike((int)auth.get("id"),"chat",chat.getId());
+                isFavorite = favoriteService.isFavorite((int)auth.get("id"),"chat",chat.getId());
+            }
+
+            chatObj.put("isLike",(isLike>0));
+            chatObj.put("isFavorite",(isFavorite>0));
+
+            chats.add(chatObj);
+        }
+
         //创建返回数据
         HashMap<String,Object> sendData = new HashMap<>();
-        sendData.put("total", total);
-        sendData.put("chats", chats);
+        sendData.put("total",total);
+        sendData.put("chats",chats);
 
         //返回我的话题数据
         return new ResponseEntity<>(sendData, HttpStatus.OK);
